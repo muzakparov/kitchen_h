1e3a3eb5ec0c7d0d8043b6fb614daeccf1f63ff5
Bakhtiiar Muzakparov <muzakparov@gmail.com>
2019-10-06 03:51:08 +0300

    Update hooks.js
    add elint disable to silence warning

--- src/hooks.js
+++ src/hooks.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 
 const useFlatListUpdate = () => {
   const [toggle, setToggle] = useState(false);
-
+  
+  //eslint-disable */
   useEffect(() => {
     setToggle(t => !t);
   }, Array.prototype.slice.call(arguments));
